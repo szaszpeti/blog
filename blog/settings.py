51f66d9774e0cda3cceeb02f2953a34fe069942b
szaszpeti <szaszakpeti@gmail.com>
2021-03-11 16:50:41 +0100

    second commit

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,5 +129,4 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+
